d4ffb552e20af82a402d9d1a7a30b31647776ff6
nwren1337 <nwren92@gmail.com>
2026-02-20 01:35:43 -0800

    improve output

--- build.sh
+++ build.sh
@@ -4,15 +4,12 @@ set -e
 STATIC="static"
 
 # Minify CSS
-npx esbuild "$STATIC/index.css" --minify --outfile="$STATIC/index.css" --allow-overwrite
-echo "CSS minified"
-
+npx esbuild "$STATIC/index.css" --minify --outfile="$STATIC/index.css" --allow-overwrite && echo "CSS minified" || echo "CSS minification failed, skipping..."
+git push --set-upstream origin improvedeploy
 # Minify JS
-npx esbuild "$STATIC/app.js" --minify --outfile="$STATIC/app.js" --allow-overwrite
-echo "JS minified"
+npx esbuild "$STATIC/app.js" --minify --outfile="$STATIC/app.js" --allow-overwrite && echo "JS minified" || echo "JS minification failed, skipping..."
 
 # Minify HTML (collapse whitespace)
-sed -i 's/^[[:space:]]*//;s/[[:space:]]*$//;/^$/d' "$STATIC/index.html"
-echo "HTML minified"
+sed -i 's/^[[:space:]]*//;s/[[:space:]]*$//;/^$/d' "$STATIC/index.html" && echo "HTML minified" || echo "HTML minification failed, skipping..."
 
 echo "Build complete"
